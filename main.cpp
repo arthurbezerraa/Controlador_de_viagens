4a2a9f201626da425a363e56aa7a657b28f716e6
arthurbezerraa <bezerra.arthur0700@gmail.com>
2024-12-18 18:55:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ void menu();
 void limparTela();
 void cadastroCidade();
 void cadastroTrajeto();
+void cadastroTransporte();
+void cadastroPassageiro();
 
 int main(){
     int opcao;
@@ -34,10 +36,10 @@ int main(){
             cadastroTrajeto();
             break;
         case 3:
-            /* code */
+            cadastroTransporte();
             break;
         case 4:
-            /* code */
+            cadastroPassageiro();
             break;
         case 5:
             /* code */
@@ -156,9 +158,9 @@ void cadastroTrajeto(){
         return;
     }
 
-        cout << endl << "Digite o tipo do trajeto ('A' para aquatico e 'T' para terrestre): ";
-        cin >> tipoTrajeto;
-        tipoTrajeto = toupper(tipoTrajeto);
+    cout << endl << "Digite o tipo do trajeto ('A' para aquatico e 'T' para terrestre): ";
+    cin >> tipoTrajeto;
+    tipoTrajeto = toupper(tipoTrajeto);
     
     while(true){
         if(tipoTrajeto == 'A' || tipoTrajeto == 'T'){
@@ -189,13 +191,14 @@ void cadastroTrajeto(){
     if(trajeto.getOrigem()->getNome().empty() || trajeto.getDestino()->getNome().empty()) // tratamento de erro para verificar se foi possível cadastrar o trajeto
     { 
         limparTela();
-        cout << "Erro ao Cadastrat trajeto. Tente novamente"<< endl;
+        cout << "Erro ao Cadastrar trajeto. Tente novamente"<< endl;
+        return;
     }
     else 
     {
         ofstream arqTrajeto("trajetos.csv", ios::app); // abre o arquivo para escrita - 'ios::app' permite que linhas sejam adicionadas ao invés de sobrescrever o conteúdo do arquivo
 
-        if (!arqCidades.is_open()) { // verificar se o arquivo foi aberto corretamente
+        if (!arqTrajeto.is_open()) { // verificar se o arquivo foi aberto corretamente
             cerr << "Erro ao abrir o arquivo.\n";
             return;
         }
@@ -207,5 +210,137 @@ void cadastroTrajeto(){
         cout << "Trajeto de '"<< trajeto.getOrigem()->getNome() << "' para '" << trajeto.getDestino()->getNome() << "' adicionado com sucesso." << endl;
 
         arqTrajeto.close();
+    }
+}
+void cadastroTransporte(){
+    string nome, localAtual;
+    char tipoTransporte;
+    int capacidade, velocidade, distancia_descanso, tempo_descanso;
+    limparTela();
+
+    cout << "Digite o nome do transporte: ";
+    getline(cin, nome);
+
+    cout << "Digite o tipo do transporte ('A' para aquatico e 'T' para terrestre): ";
+    cin >> tipoTransporte;
+    tipoTransporte = toupper(tipoTransporte);
+    
+    while(true){
+        if(tipoTransporte == 'A' || tipoTransporte == 'T'){
+            break;
+        }else{
+            cout << endl << "Tipo invalido" << endl;
+            cout << "Digite o tipo do transporte ('A' para aquatico e 'T' para terrestre): ";
+            cin >> tipoTransporte;
+            tipoTransporte = toupper(tipoTransporte);
+        }
+    }
+
+    cout << "Digite a capacidade do transporte: ";
+    cin >> capacidade;
+
+    while (capacidade <= 0){   
+        cout << endl << "Capacidade invalida" << endl;
+        cout << "Digite a capacidade do transporte: ";
+        cin >> capacidade;
+    }
+
+    cout << "Digite a velocidade do transporte (em km/h): ";
+    cin >> velocidade;
+
+    while (velocidade <= 0){   
+        cout << endl << "Velocidade invalida" << endl;
+        cout << "Digite a velocidade do transporte (em km/h): ";
+        cin >> velocidade;
+    } 
+
+    cout << "Digite a distancia de descanso do transporte (em km): ";
+    cin >> distancia_descanso;
+
+    while (distancia_descanso <= 0){   
+        cout << endl << "Distancia invalida" << endl;
+        cout << "Digite a distancia de descanso do transporte (em km): ";
+        cin >> distancia_descanso;
+    }
+
+    cout << "Digite o tempo de descanso do transporte (em h): ";
+    cin >> tempo_descanso;
+
+    while (tempo_descanso <= 0){   
+        cout << endl << "Tempo invalido" << endl;
+        cout << "Digite o tempo de descanso do transporte (em h): ";
+        cin >> tempo_descanso;
+    }
+
+    /*
+    cout << "Digite o local atual do transporte: ";
+    getline(cin, localAtual);
+
+    ifstream arqCidades("cidades.csv");
+    if (!arqCidades.is_open()) {
+        limparTela();
+        cerr << "Erro ao abrir o arquivo 'cidades.csv'" << endl;
+        return;
+    }
+
+    string linha;
+    bool verificaLocalAtual = false;
+
+    while(getline(arqCidades, linha)){
+        if(localAtual == linha){
+            verificaLocalAtual = true;
+            break;
+        }
+    }
+
+    if(verificaLocalAtual == false){
+        limparTela();
+        cout << "Cidade '" << localAtual << "' nao foi cadastrada" << endl;
+        return;
+    }
+    */
+
+    Transporte transporte(nome, tipoTransporte, capacidade, velocidade, distancia_descanso, tempo_descanso);
+    if(transporte.getNome().empty()){
+        cout << "Erro ao Cadastrar transporte. Tente novamente"<< endl;
+    }else{
+        ofstream arqTransporte("transportes.csv", ios::app); // abre o arquivo para escrita - 'ios::app' permite que linhas sejam adicionadas ao invés de sobrescrever o conteúdo do arquivo
+
+        if (!arqTransporte.is_open()) { // verificar se o arquivo foi aberto corretamente
+            cerr << "Erro ao abrir o arquivo.\n";
+            return;
+        }
+
+        arqTransporte << transporte.getNome() << "," << transporte.getTipoTransporte() << "," << transporte.getCapacidadePassageiros() << "," << transporte.getVelocidade() << "," << transporte.getDistanciaDescanso() << "," << transporte.getTempoDescanso() << "," << transporte.getLocalAtual() << "\n";
+
+        limparTela();
+        cout << "transporte '" << transporte.getNome() << "' adicionado com sucesso. " << endl;
+        arqTransporte.close();
+    }
+}
+
+void cadastroPassageiro(){
+    limparTela();
+
+    string nome;
+    cout << "Digite o nome do passageiro: ";
+    getline(cin, nome);
+
+    Passageiro passageiro(nome);
+
+    if(passageiro.getNome().empty())
+    {
+        limparTela();
+        cout << "Erro ao adicionar passageiro. Tente novamente"<< endl;
+    }else
+    {
+        ofstream arqPassageiros("passageiros.csv", ios::app);
+
+        arqPassageiros << passageiro.getNome() << "," << passageiro.getLocalAtual() << "\n";
+
+        limparTela();
+        cout << "Passageiro '"<< passageiro.getNome() << "' cadastrado com sucesso." << endl;
+
+        arqPassageiros.close();
     }
 }
